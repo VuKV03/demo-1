246220c3426bbd3885ca230051214c4ec31a1b81
Khuất Văn Vũ <khuatvanvu03@gmail.com>
2024-08-30 09:30:47 +0700

    build: update rest parameters

--- Rest-Parameters/script.js
+++ Rest-Parameters/script.js
@@ -1,7 +1,7 @@
-const number = (...numOther) => {
-  // console.log("num1: ", num1)
-  // console.log("num2: ", num2)
-  console.log("Other: ", numOther)
+const number = (num1, num2, ...numOther) => {
+  console.log("num1: ", num1) // 1
+  console.log("num2: ", num2) // 2
+  console.log("Other: ", numOther) // [3, 4, 5, 6]
 }
 
 number(1, 2, 3, 4, 5, 6);
