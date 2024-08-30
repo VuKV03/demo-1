e4d3f225fa22c35032bbc785f77bd225139ab6b4
Khuất Văn Vũ <khuatvanvu03@gmail.com>
2024-08-30 09:35:41 +0700

    build: update destructuring

--- Destructuring/script.js
+++ Destructuring/script.js
@@ -2,17 +2,16 @@
 const arr = [1, 2, 3, 4, 5];
 const [n1, n2] = arr;
 
-console.log(n1);
-console.log(n2);
-
+console.log(n1); // 1
+console.log(n2); // 2
 
 // Object
 const obj = {
   o1: 1,
   o2: 2,
   o3: 3,
-}
-const {o1, o2} = obj;
+};
+const { o1, o2 } = obj;
 
-console.log(o1);
-console.log(o2);
+console.log(o1); // 1
+console.log(o2); // 2
