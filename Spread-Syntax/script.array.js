0af675063f4f4263529b284a3d4e511fa638c298
Khuất Văn Vũ <khuatvanvu03@gmail.com>
2024-08-30 09:21:55 +0700

    build: update spread syntax

--- Spread-Syntax/script.array.js
+++ Spread-Syntax/script.array.js
@@ -1,5 +1,5 @@
 const arr1 = [1, 2, 3];
 const arr2 = [...arr1];
 
-console.log(arr1)
-console.log(arr2)
+console.log(arr1) // [1, 2, 3]
+console.log(arr2) // [1, 2, 3]
--- Spread-Syntax/script.object.js
+++ Spread-Syntax/script.object.js
@@ -3,8 +3,7 @@ const obj1 = {
   key2: 2,
   key3: 3,
 };
-
 const obj2 = { ...obj1 };
 
-console.log(obj1);
-console.log(obj2);
+console.log(obj1); // {key1: 1, key2: 2, key3: 3}
+console.log(obj2); // {key1: 1, key2: 2, key3: 3}
